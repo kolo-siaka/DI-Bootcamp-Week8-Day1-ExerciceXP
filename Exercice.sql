5031635bbca9a99ea25dc21f89688629e0f84689
SORO <kolosiaka@gmail.com>
2023-02-15 19:36:31 +0100

    'w8d1'

---
+++ Exercice.sql
@@ -0,0 +1,30 @@
+--Add the following items to the items table
+--CREATE TABLE items(
+--   id SERIAL PRIMARY KEY, 
+--   name VARCHAR NOT NULL,
+--	price VARCHAR
+--);
+--CREATE TABLE customers(
+--   id SERIAL PRIMARY KEY, 
+--   name VARCHAR NOT NULL,
+--   lastname VARCHAR NOT NULL
+--);
+--Add 5 new customers to the customers table:
+--INSERT INTO items (name ,price) VALUES ('Small Desk','100');
+--INSERT INTO items (name ,price) VALUES ('Large desk','300');
+--INSERT INTO items (name ,price) VALUES ('Fan','80');
+--INSERT INTO customers (name ,lastname) VALUES ('Greg','Jones');
+--INSERT INTO customers (name ,lastname) VALUES ('Sandra','Jones');
+--INSERT INTO customers (name ,lastname) VALUES ('Scott','Scott');
+--INSERT INTO customers (name ,lastname) VALUES ('Trevor','Green');
+--INSERT INTO customers (name ,lastname) VALUES ('Melanie','Johnson');
+--Use SQL to fetch the following data from the database:
+--1 SELECT * FROM items ;
+--SELECT * FROM items WHERE price > 80 ;
+--SELECT * FROM items WHERE price < 300 ;
+--SELECT * FROM customers WHERE lastname = 'Smith';
+--SELECT * FROM customers WHERE lastname = 'Jones';
+--SELECT * FROM customers WHERE lastname NOT LIKE 'Scott';
+
+
+
